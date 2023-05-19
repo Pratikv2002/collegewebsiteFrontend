f976c269cec10c33b2f3aa63d0309c523546ddeb
Pratik Vyavahare <89389983+Pratikv2002@users.noreply.github.com>
2023-05-20 00:31:41 +0530

    Google map is Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -15,7 +15,6 @@ function Home() {
         <div onClick={()=>{navigate("/googleMap")}} className = "px-5 py-3 rounded-left" style={{position:'absolute',backgroundColor:"white",right:0,border:"solid 2px blue",borderTopLeftRadius:"10px",borderBottomLeftRadius:"10px"}}><svg color='blue' xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
 </svg>location</div>
-<Link to="/adminRegister">Register</Link>
        <SocialMedia/>
         <Footer/>
     </div>
